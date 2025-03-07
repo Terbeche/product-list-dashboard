2d79bf2e6b4c20dcc006e27bc8446974522d62d8
mostefa <termus96@gmail.com>
2025-03-08 00:08:38 +0100

    Setup the App component for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+
+import { productList } from './data';
 
 function App() {
+  const [activeCategory, setActiveCategory] = useState("all");
+  const products = productList.data.products;
+
 
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Header 
+        activeCategory={activeCategory} 
+        setActiveCategory={setActiveCategory} 
+      />
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to={`/category/${activeCategory}`} />} />
+          <Route 
+            path="/category/:categoryName" 
+            element={
+              <ProductListing 
+                products={products} 
+                activeCategory={activeCategory}
+                setActiveCategory={setActiveCategory}
+              />
+            } 
+          />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
